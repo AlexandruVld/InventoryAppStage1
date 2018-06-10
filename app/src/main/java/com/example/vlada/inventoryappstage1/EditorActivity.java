9774fc8a87fd861cffa41aa06fe6950820037bff
Solidfire <34316981+AlexandruVld@users.noreply.github.com>
2018-06-10 10:56:03 +0300

    Initial commit

--- app/src/main/java/com/example/vlada/inventoryappstage1/EditorActivity.java
+++ app/src/main/java/com/example/vlada/inventoryappstage1/EditorActivity.java
@@ -55,6 +55,7 @@ public class EditorActivity extends AppCompatActivity {
         setupSpinner();
     }
 
+    // Setup the dropdown spinner that allows the user to select the availability of the product.
     private void setupSpinner() {
         // Create adapter for spinner. The list options are from the String array it will use
         // the spinner will use the default layout
